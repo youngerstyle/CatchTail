7885db35e26a6645d1ecedfef21670394f30a117
xieyizhi <594xieyizhi@gmail.com>
2026-05-19 11:33:00 +0800

    Publish CatchTail as plugin root

--- plugins/catchtail/bin/catchtail.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import { runCliMain } from "../src/cli.js";
-
-await runCliMain();
--- plugins/catchtail/hooks/catchtail-hook.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import { runHookCli } from "../src/hook.js";
-
-await runHookCli();
